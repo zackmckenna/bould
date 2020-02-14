c4619e7d8baa541bd7de97ed43a645884cc6e109
Zack McKenna <zackmckenna@gmail.com>
2020-02-14 15:45:07 -0500

    git init

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
       </header>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ marginTop: '12vh', marginBottom: '0', fontSize: '10vw'}}><strong>Bould.</strong></h1>
+        <p>a minimalist climb-tracking app</p>
+      </div>
+      <div style={{ paddingTop: '1rem', justifyContent: 'center', display: 'flex' }}>
+        <span style={{ backgroundColor: '#26547C'}} class='dot'></span>
+        <span style={{ backgroundColor: '#EF476F' }} class='dot'></span>
+        <span style={{ backgroundColor:'#FFD166' }} class='dot'></span>
+        <span style={{ backgroundColor:'#06D6A0' }} class='dot'></span>
+        <span style={{ backgroundColor:'#59c75d' }} class='dot'></span>
+      </div>
     </div>
   );
 }
